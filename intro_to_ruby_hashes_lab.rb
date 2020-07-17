743c882659fc726b81031f9bd5855deeb08f1fab
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-17 08:25:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,6 +46,7 @@ def monopoly_with_fourth_tier
 		monopoly= {:railroads =>
  	{:pieces => 4,
  	:names => {
+ 	  
  	  :reading_railroad =>{ 
  	    :mortgage_value => '$100'
     },
@@ -58,12 +59,13 @@ def monopoly_with_fourth_tier
  	  :shortline =>{
  	    :mortgage_value => "$800"
  	},
- 	
- 	:rent_in_dollars => {
+ 	  :rent_in_dollars => {
  	  :one_piece_owned => 25, 
  	  :two_pieces_owned => 50, 
  	  :three_pieces_owned =>100, 
  	  :four_pieces_owned => 200
+ 	  
+ 	
  	  }
   }
 }
